04f328f79087f494a3ab0a10165294923e071627
DingoMC <95879619+DingoMC@users.noreply.github.com>
2022-11-09 10:34:33 +0100

    Update main.c

--- Lab3/main.c
+++ Lab3/main.c
@@ -9,12 +9,25 @@ int main(void)
 {
     _delay_ms(15);
 	LCD_init();
-	/*
 	pisz_1("Test W1");
 	pisz_2("Test W2");
-	_delay_ms(5000);
-	LCD_clear();*/
-	pisz_xy("Elo", 0, 5);
+	_delay_ms(3000);
+	LCD_clear();
+	_delay_ms(500);
+	pisz_xy("Test XY", 0, 5);
+	pisz_xy("Test 2", 1, 3);
+	_delay_ms(3000);
+	LCD_clr_1();
+	_delay_ms(500);
+	LCD_clr_2();
+	_delay_ms(500);
+	pisz_xy("Test Clear XY", 0, 1);
+	_delay_ms(3000);
+	LCD_clr_xy(0, 5);
+	_delay_ms(500);
+	LCD_clear();
+	_delay_ms(500);
+	pisz_xy("Test OK", 1, 4);
     while (1) 
     {
     }
