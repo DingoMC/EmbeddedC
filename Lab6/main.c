8fecd2c2f76c51f170f9e928e985a5cdec8f79ec
DingoMC <95879619+DingoMC@users.noreply.github.com>
2022-11-29 17:04:34 +0100

    Update main.c

--- Lab6/main.c
+++ Lab6/main.c
@@ -17,20 +17,20 @@ void UART_Init (void) {
 	UBRRH = (BAUD_PRESCALE >> 8);
 }
 
-unsigned char UART_RxChar () {
-	while ((UCSRA & (1 << RXC)) == 0);
-	return(UDR);
+unsigned char UART_GetChar () {
+	while (!(UCSRA & (1 << RXC)));
+	return UDR;
 }
 
-void UART_TxChar (char ch) {
-	while (! (UCSRA & (1<<UDRE)));
+void UART_SendChar (char ch) {
+	while (!(UCSRA & (1<<UDRE)));
 	UDR = ch;
 }
 
 void UART_SendString (char *str) {
 	unsigned char c = 0;
 	while (str[c] != 0) {
-		UART_TxChar(str[c]);
+		UART_SendChar(str[c]);
 		c++;
 	}
 }
@@ -40,7 +40,7 @@ static unsigned cnt = 0;
 ISR (TIMER0_COMP_vect) {
 	if (++cnt == L0_DLY / 20) {
 		cnt = 0;
-		PORTA ^= 0b00000001;
+		PORTA ^= 0x01;
 	}
 }
 
@@ -50,42 +50,33 @@ ISR (USART_RXC_vect) {
 	if ((int) x >= 49 && (int) x <= 55) {
 		PORTA ^= 0b00000001 << (x - 48);
 		UART_SendString("Wcisnieto [");
-		UART_TxChar(x);
+		UART_SendChar(x);
 		UART_SendString("]\r\n");
 	}
-	else if (x == 'c') {
-		PORTA = 0b00000000;
-	}
-	else if (x == 's') {
-		PORTA = 0b11111111;
-	}
-	else {
-		UART_SendString("Bledny klawisz!\r\n");
-	}
+	else if (x == 'c') PORTA &= 0b00000001;
+	else if (x == 's') PORTA |= 0b11111110;
+	else UART_SendString("Bledny klawisz!\r\n");
 }
 
 int main(void) {
 	// Zadanie 1
-    UART_Init();
-    UART_SendString("Marcin Basak\r\n");
+	UART_Init();
+	UART_SendString("Marcin Basak\r\n");
 	
 	// Zadanie 2
 	char c;
 	UART_SendString("Nacisnij klawisz [d]: ");
-	c = UART_RxChar();
+	c = UART_GetChar();
 	if (c == 'd') UART_SendString("Znak OK\r\n");
 	else UART_SendString("Bledny Znak!\r\n");
 	
 	// Zadanie 3
 	DDRA = 0b11111111;	// LED in
 	PORTA = 0b00000000;	// Led wył.
-	TCCR0 |= 1 << WGM01;
-	TCCR0 &= ~(1 << WGM00);
-	TCCR0 |= (1 << CS02) | (1 << CS00);
-	TCCR0 &= ~(1 << CS01);
-	OCR0 = 156;
+	TCCR0 |= (1 << WGM01) | (1 << CS02) | (1 << CS00);
 	TIMSK |= 1 << OCIE0;
 	TIFR |= 1 << OCF0;
+	OCR0 = 78;
 	sei();
     while (1) {
 		
