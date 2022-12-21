0942d00ecbfd9bf2e6274c95b1f83a64eb197943
DingoMC <95879619+DingoMC@users.noreply.github.com>
2022-12-21 10:36:34 +0100

    Update Zadanie2.c

--- Lab10/Zadanie2.c
+++ Lab10/Zadanie2.c
@@ -1,15 +1,25 @@
-int main () {
-  MCK = 48000000;
-  
-  // CPIV inkrementowany 48MHz/16 = 3 miliony razy na sekunde
-  // Odliczenie 300000 inkrementacji zajmie 0.1s (zapisujemy w sekcji PIV rejestru PIT_MR)
-  PIT_MR = 300000 | 1 << 24;
-  // Po wykonaniu 50 takich odliczeń otrzymamy 0.1s* 50 = 5s
-  // Czekamy zatem na doliczenie do wartości 50 w PICNT
-  while (1) {
-    if (PICNT == 50) {
-      // Kod co 5s
-      PICNT = 0;
-    }
-  }
+#include <targets/AT91SAM7X256.h>
+
+int main(void){
+	PMC_PCER = 1 << 3;
+	PIOB_PER = 1 << 20;
+	PIOB_OER = 1 << 20;		// PB20 jako wyjscie
+	PIOB_OWER = 1 << 20;
+	PIT_PIVR;
+	PIT_MR = 299999 | (1 << 24);
+	/*
+	MCR = 48MHz
+	48MHz / 16 = 3MHz 3 miliony razy na sekunde
+	PIT_MR = 299999 (300000 odliczen zajmie 0.1s)
+	przepelnienie co 0.1s
+	5s odiczymy zatem 5 / 0.1 = 50 razy
+	inkrementacja PICNT do 50
+	*/
+	while(1){
+		if(PIT_PIIR >= (50 << 20)){
+			PIT_PIVR;
+			PIOB_ODSR ^= 1 << 20;
+		}
+	}
+	return 0;
 }
